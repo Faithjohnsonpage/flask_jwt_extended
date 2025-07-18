a446ff707f45fa5feb73c80d00b8fb2a6184c907
Faith Johnson <ermac_4life@yahoo.com>
2025-07-18 18:16:15 +0100

    Introduced alembic for database migration for the application

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -4,6 +4,8 @@ Initialize the models package
 """
 from models.engine.db import DB
 
+# Import all model classes here so Alembic can see them
+from models.user import User
 
 storage = DB()
 storage.reload()
